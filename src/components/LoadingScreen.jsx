b36e7037d6db1cfbd8befcb247e028ba6b187652
Kwon Zhu <kwonzhu@gmail.com>
2025-03-22 23:55:49 +1030

    feat: add a LoadingScreen (typewriter and progress bar)

---
+++ src/components/LoadingScreen.jsx
@@ -0,0 +1,45 @@
+import { useEffect, useState } from "react";
+
+export const LoadingScreen = ({ onComplete }) => {
+  const [text, setText] = useState("");
+  const fullText = "<Welcome To My Space/>"; //the end state
+
+  useEffect(() => {
+    let index = 0; //the letter is being typed
+    //rerun every 100ms
+    const interval = setInterval(() => {
+      setText(fullText.substring(0, index)); //every time the interval runs, it will display one extra letter
+      index++;
+
+      // Stop the interval and (1 second after) hide the LoadingScreen When the entire text is typed out
+      if (index > fullText.length) {
+        clearInterval(interval);
+
+        setTimeout(() => {
+          onComplete();
+        }, 1000);
+      }
+    }, 100);
+
+    return () => clearInterval(interval); // to prevent memory leaks if the component unmounts
+  }, [onComplete]); //it runs once on the first render, or if onComplete changed
+
+  return (
+    <div className="fixed inset-0 z-50 bg-black text-gray-100 flex flex-col justify-center items-center">
+      {/* The 'inset' class sets the distance between an element and the parent element (t/b/l/r) */}
+
+      {/* typewriter */}
+      <div className="mb-4 text-4xl font-mono font-bold">
+        {text}
+        {/* Blinking cursor */}
+        <span className="animate-blink ml-1"> | </span>
+      </div>
+
+      {/* progress bar */}
+      <div className="w-[200px] h-[2px] bg-gray-800 rounded relative overflow-hidden">
+        {/* Moving bar with glowing effect */}
+        <div className="w-[40%] h-full bg-blue-500 shadow-[0_0_15px_3b82f6] animate-loading-bar"></div>
+      </div>
+    </div>
+  );
+};
